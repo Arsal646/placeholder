48716d934b12f0db14a1ab26db4ea88d16b6d11b
Arslan Iftikhar <arsalmalik1920@gmail.com>
2025-06-20 15:05:43 -0800

    dist

--- dist/image-placeholder/server/angular-app-manifest.mjs
+++ dist/image-placeholder/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 4655, hash: 'c2e821af4b3b76b6b8c291db90c230dd1ce70b2f60286a10f93caf985272f2db', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1013, hash: '1114bc2fe38c12527d39018ad266e11ae01690beee99b38ad9620bbf3761ac38', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 20133, hash: 'e7513fbea0b4eaf420d97a5a2c1fcbcdb103a9ff89abc75ca2022492737e6cf1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-JF2R5OQN.css': {size: 14036, hash: '9KhSC7cHF88', text: () => import('./assets-chunks/styles-JF2R5OQN_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 6036, hash: 'bbca335fe6d91c80f1228242b8b152d1f00195365dab42c19b15529407467644', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1013, hash: '555fc00ee4ccaa20ebb31cac12c31499b4f26f6375081170850b350437ce9b7d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 21588, hash: 'de9733d376dd747eabc640fdce8540ee3bb9b1466fbc6d88a50667d05f3794ea', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-IYSUJ4WY.css': {size: 16379, hash: '59rkLmDL8y4', text: () => import('./assets-chunks/styles-IYSUJ4WY_css.mjs').then(m => m.default)}
   },
 };
